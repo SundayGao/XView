aa1a9faf51344e0bff18b03f0d40f7c3c3bb5e1e
gaoyuanwu <gaoyuanwu@fangdd.com>
2019-08-05 15:15:12 +0800

    add switch activity

--- app/src/main/java/com/gyw/xview/MainActivity.kt
+++ app/src/main/java/com/gyw/xview/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.gyw.xview
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
+/**
+ * @Author: gaoyuanwu
+ * @Date: 2019-08-05 14:59
+ * @Description:
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bt_switch_view.setOnClickListener {
+            startActivity(Intent(this, SwitchActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/gyw/xview/SwitchActivity.kt
@@ -0,0 +1,17 @@
+package com.gyw.xview
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * @Author: gaoyuanwu
+ * @Date: 2019-08-05 14:32
+ * @Description:
+ */
+class SwitchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_switch)
+    }
+}
